549847c5d3132ac1c593232b2803cc22bd5363ce
Stanley <aynbunda@gmail.com>
2021-09-20 12:37:44 -0400

    post.js works and post come up

--- client/src/components/posts/posts.js
+++ client/src/components/posts/posts.js
@@ -3,6 +3,7 @@ import { Grid, CircularProgress } from '@material-ui/core'
 import { useSelector } from 'react-redux';
 import Post from './Post/Post';
 import useStyles from './styles'
+
 const Posts = () => {
     const posts = useSelector((state) => state.posts);
     const classes = useStyles();
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,12 +1,13 @@
 export default (posts = [], action) => {
     switch (action.type){
-        case 'FETCH_ALL':
+
+        case 'FETCH_ALL' :
             return action.payload;
 
-        case 'CREATE':
+        case 'CREATE' :
             return [...posts, action.payload];
         default:
             return posts;
     }
 
-}
+};
